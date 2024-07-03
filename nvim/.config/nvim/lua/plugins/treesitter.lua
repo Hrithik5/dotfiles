003d8e2235f19e66fdeb9f9a893b7da320991df1
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2024-07-03 18:55:09 +0530

    Updated treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,14 +2,22 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    -- treesitter configs
-    local config = require("nvim-treesitter.configs")
-      config.setup({
-          ensure_installed = {"lua","typescript", "python"},
-          auto_install = true,
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "typescript",
+        "python",
+        "go",
+        "terraform",
+        "cpp",
+        "bash",
+        "yaml"
+      },
+      auto_install = true,
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
     })
-  end
+  end,
 }
+
