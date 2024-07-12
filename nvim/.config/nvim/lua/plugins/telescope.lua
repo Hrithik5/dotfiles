23e4654f7a00ef4e059483f14a4af710d08f63bc
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2024-07-12 18:35:15 +0530

    Fixed telescope and nvimtree config and added new todo-comment plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,38 +1,86 @@
 return {
- {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-         end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "ThePrimeagen/harpoon",
-    },
-    config = function()
-      -- telescope key-bindings
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files"})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep in Files"})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Show items in Buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help utility" })
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Recently opened Files" })
-      vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
-      vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
-      vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+	-- Telescope UI Select Extension
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 
-      require("telescope").load_extension("ui-select")
-      require("telescope").load_extension("noice")
+	-- Main Telescope Configuration
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"ThePrimeagen/harpoon",
+		},
+		config = function()
+			-- Setup Telescope
+			require("telescope").setup({
+				defaults = {
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+					},
+					prompt_prefix = "> ",
+					selection_caret = "> ",
+					entry_prefix = "  ",
+					initial_mode = "insert",
+					selection_strategy = "reset",
+					sorting_strategy = "descending",
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							mirror = false,
+						},
+						vertical = {
+							mirror = false,
+						},
+					},
+					file_sorter = require("telescope.sorters").get_fuzzy_file,
+					file_ignore_patterns = {},
+					generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+					path_display = { "absolute" },
+					winblend = 0,
+					border = {},
+					borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
+					color_devicons = true,
+					use_less = true,
+					set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+					file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+					grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+					qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+					buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+				},
+			})
 
-    end,
-  },
+			-- Telescope Key Bindings
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep in Files" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Show items in Buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help utility" })
+			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Recently opened Files" })
+			vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
+			vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+
+			-- Load Extensions
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("noice")
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/todo-comment.lua
+++ nvim/.config/nvim/lua/plugins/todo-comment.lua
@@ -1,21 +1,38 @@
 return {
-  "folke/todo-comments.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local todo_comments = require("todo-comments")
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo_comments = require("todo-comments")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- Keymap settings
+		local keymap = vim.keymap
 
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
+		-- Jump to next todo comment
+		keymap.set("n", "]c", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
 
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
+		-- Jump to previous todo comment
+		keymap.set("n", "[c", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
 
-    todo_comments.setup()
-  end,
+		-- Open a list of todo comments
+		keymap.set("n", "<leader>td", ":TodoTrouble<CR>", { desc = "Open todo list in Trouble" })
+		keymap.set("n", "<leader>cl", ":TodoLocList<CR>", { desc = "Open todo list in Location List" })
+		keymap.set("n", "<leader>cq", ":TodoQuickFix<CR>", { desc = "Open todo list in Quickfix" })
+		keymap.set("n", "<leader>ct", ":TodoTelescope<CR>", { desc = "Search todos with Telescope" })
+
+		-- Setup todo-comments with default configuration
+		todo_comments.setup()
+	end,
 }
+
+-- TODO: This is a todo comment
+--FIX: This is a fix comment
+--HACK: This is a hack comment
+--WARN: This is a warning comment
+--PERF: This is a performance comment
+--NOTE: This is a note comment
+--TEST: This is a test comment ]]
