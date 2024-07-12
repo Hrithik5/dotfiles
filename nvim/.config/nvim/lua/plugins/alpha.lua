221e3744610bf28fa5ab73bd60b18fed2c1387a7
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2024-07-12 21:02:46 +0530

    Fixed old config by refactoring the code

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,31 +1,36 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+		-- Set header
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
 
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("n", "  > New File", "<cmd>ene<CR>"),
+			dashboard.button("e", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("sr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+		}
 
-    alpha.setup(dashboard.opts)
-  end,
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,38 +1,52 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim"
-  },
-  config = function()
-    local harpoon = require("harpoon").setup()
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		local harpoon = require("harpoon").setup()
 
-      require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-          results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-      }):find()
-    end
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
 
-    -- Harpoon related key bindings
-    vim.keymap.set("n", "<leader>hl", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Add to harpoon list" })
-    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Move to previous in harpoon list" })
-    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Move to next in harpoon list" })
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = require("telescope.config").values.file_previewer({}),
+					sorter = require("telescope.config").values.generic_sorter({}),
+				})
+				:find()
+		end
 
-    -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-  end
+		-- Harpoon related key bindings
+		vim.keymap.set("n", "<leader>hl", function()
+			toggle_telescope(harpoon:list())
+		end, { desc = "Open Harpoon window" })
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+		end, { desc = "Add to Harpoon list" })
+		vim.keymap.set("n", "<leader>hr", function()
+			harpoon:list():remove()
+		end, { desc = "Remove from Harpoon list" })
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end, { desc = "Move to previous in Harpoon list" })
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end, { desc = "Move to next in Harpoon list" })
+
+		-- Uncomment the following lines if you want to directly select items by number
+		-- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+		-- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+		-- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+		-- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+	end,
 }
