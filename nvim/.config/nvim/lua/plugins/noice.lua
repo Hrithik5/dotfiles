a69306afb2487ed9054b64f1ed316a82bcc410b9
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2025-02-05 18:08:35 +0530

    Changed config for noice and wilder for easy access

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,20 +1,160 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-   --[[  routes = {
-      {
-        filter = { event = "notify", find = "No infomation available" },
-        opts = {skip = true},
-      },
-    },
-    presets = {
-      lsp_doc_border = true,
-    }, ]]
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  },
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		cmdline = {
+			enabled = true, -- enables the Noice cmdline UI
+			view = "cmdline", -- set to "cmdline" for bottom position
+			opts = {},
+			format = {
+				cmdline = { pattern = "^:", icon = "", lang = "vim" },
+				search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+				lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+				input = { view = "cmdline_input", icon = "󰥻 " },
+			},
+		},
+		messages = {
+			enabled = true,
+			view = "notify",
+			view_error = "notify",
+			view_warn = "notify",
+			view_history = "messages",
+			view_search = "virtualtext",
+		},
+		popupmenu = {
+			enabled = true,
+			backend = "nui",
+			kind_icons = {},
+		},
+		redirect = {
+			view = "popup",
+			filter = { event = "msg_show" },
+		},
+		commands = {
+			history = {
+				view = "split",
+				opts = { enter = true, format = "details" },
+				filter = {
+					any = {
+						{ event = "notify" },
+						{ error = true },
+						{ warning = true },
+						{ event = "msg_show", kind = { "" } },
+						{ event = "lsp", kind = "message" },
+					},
+				},
+			},
+			last = {
+				view = "popup",
+				opts = { enter = true, format = "details" },
+				filter = {
+					any = {
+						{ event = "notify" },
+						{ error = true },
+						{ warning = true },
+						{ event = "msg_show", kind = { "" } },
+						{ event = "lsp", kind = "message" },
+					},
+				},
+				filter_opts = { count = 1 },
+			},
+			errors = {
+				view = "popup",
+				opts = { enter = true, format = "details" },
+				filter = { error = true },
+				filter_opts = { reverse = true },
+			},
+			all = {
+				view = "split",
+				opts = { enter = true, format = "details" },
+				filter = {},
+			},
+		},
+		notify = {
+			enabled = true,
+			view = "notify",
+		},
+		lsp = {
+			progress = {
+				enabled = true,
+				format = "lsp_progress",
+				format_done = "lsp_progress_done",
+				throttle = 1000 / 30,
+				view = "mini",
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+				["vim.lsp.util.stylize_markdown"] = false,
+				["cmp.entry.get_documentation"] = false,
+			},
+			hover = {
+				enabled = true,
+				silent = false,
+				view = nil,
+				opts = {},
+			},
+			signature = {
+				enabled = true,
+				auto_open = {
+					enabled = true,
+					trigger = true,
+					luasnip = true,
+					throttle = 50,
+				},
+				view = nil,
+				opts = {},
+			},
+			message = {
+				enabled = true,
+				view = "notify",
+				opts = {},
+			},
+			documentation = {
+				view = "hover",
+				opts = {
+					lang = "markdown",
+					replace = true,
+					render = "plain",
+					format = { "{message}" },
+					win_options = { concealcursor = "n", conceallevel = 3 },
+				},
+			},
+		},
+		markdown = {
+			hover = {
+				["|(%S-)|"] = vim.cmd.help,
+				["%[.-%]%((%S-)%)"] = require("noice.util").open,
+			},
+			highlights = {
+				["|%S-|"] = "@text.reference",
+				["@%S+"] = "@parameter",
+				["^%s*(Parameters:)"] = "@text.title",
+				["^%s*(Return:)"] = "@text.title",
+				["^%s*(See also:)"] = "@text.title",
+				["{%S-}"] = "@parameter",
+			},
+		},
+		health = {
+			checker = true,
+		},
+		presets = {
+			bottom_search = true, -- Keeps search (`/`, `?`) at bottom
+			command_palette = false, -- Prevents floating command UI
+			long_message_to_split = false,
+			inc_rename = false,
+			lsp_doc_border = false,
+		},
+		throttle = 1000 / 30,
+		views = {},
+		routes = {},
+		status = {},
+		format = {},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
 }
--- nvim/.config/nvim/lua/plugins/wilder.lua
+++ nvim/.config/nvim/lua/plugins/wilder.lua
@@ -1,17 +1,20 @@
 return {
-  'gelguy/wilder.nvim',
-  config = function()
-    local wilder = require("wilder")
+	"gelguy/wilder.nvim",
+	config = function()
+		local wilder = require("wilder")
 
-    wilder.setup({
-      modes = {':', '/', '?'}
-    })
+		wilder.setup({
+			modes = { ":", "/", "?" },
+		})
 
-    wilder.set_option('renderer', wilder.popupmenu_renderer({
-      highlighter = wilder.basic_highlighter(),
-      left = { ' ', wilder.popupmenu_devicons() },
-      right = { ' ', wilder.popupmenu_scrollbar() },
-    }))
-
-    end,
+		wilder.set_option(
+			"renderer",
+			wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+				highlights = {
+					border = "Normal", -- highlight to use for the border
+				},
+				border = "rounded",
+			}))
+		)
+	end,
 }
