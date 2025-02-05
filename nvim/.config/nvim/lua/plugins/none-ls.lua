0ed41caf445dbec07b423e534b563a0a264fbc97
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2025-02-05 20:05:28 +0530

    Changed config for none-ls

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,5 @@
 return {
-	"nvimtools/none-ls.nvim",
+	"jose-elias-alvarez/null-ls.nvim", -- Correct plugin name (was "none-ls.nvim")
 	event = "VeryLazy",
 	dependencies = {
 		"nvimtools/none-ls-extras.nvim",
@@ -8,17 +8,22 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				-- Formatting
+				null_ls.builtins.formatting.eslint_d, -- Use either eslint_d OR eslint, not both
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.completion.spell,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.yamlfix,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.eslint_d,
 				null_ls.builtins.formatting.goimports_reviser,
 				null_ls.builtins.formatting.golines,
 				null_ls.builtins.formatting.gofumpt,
-				require("none-ls.diagnostics.eslint_d"),
+
+				-- Diagnostics (moved to proper section)
+				null_ls.builtins.diagnostics.eslint_d, -- Use builtin instead of require
+
+				-- Completion
+				null_ls.builtins.completion.spell,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format Code" })
