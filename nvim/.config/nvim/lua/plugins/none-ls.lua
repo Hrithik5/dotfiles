ca1f6516f1332537addda554f547871d4f5d01d9
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2025-03-31 12:51:14 +0530

    Replaces null-ls with none-ls

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,31 +1,32 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim", -- Correct plugin name (was "none-ls.nvim")
-	event = "VeryLazy",
+	"nvimtools/none-ls.nvim",
+  event = "VeryLazy",
 	dependencies = {
 		"nvimtools/none-ls-extras.nvim",
 	},
 	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
+		local none_ls = require("none-ls")
+		none_ls.setup({
 			sources = {
 				-- Formatting
-				null_ls.builtins.formatting.eslint_d, -- Use either eslint_d OR eslint, not both
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.yamlfix,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.golines,
-				null_ls.builtins.formatting.gofumpt,
+				none_ls.builtins.formatting.eslint_d,
+				none_ls.builtins.formatting.stylua,
+				none_ls.builtins.formatting.black,
+				none_ls.builtins.formatting.isort,
+				none_ls.builtins.formatting.yamlfix,
+				none_ls.builtins.formatting.prettier,
+				none_ls.builtins.formatting.goimports_reviser,
+				none_ls.builtins.formatting.golines,
+				none_ls.builtins.formatting.gofumpt,
 
-				-- Diagnostics (moved to proper section)
-				null_ls.builtins.diagnostics.eslint_d, -- Use builtin instead of require
+				-- Diagnostics
+				none_ls.builtins.diagnostics.eslint_d,
 
 				-- Completion
-				null_ls.builtins.completion.spell,
+				none_ls.builtins.completion.spell,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format Code" })
 	end,
 }
+
