f9ab9220a15ae6a3b4e172fa69207835c8bcb038
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2025-06-21 23:56:08 +0530

    Updated theme settings and other pluging

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,14 @@
 -- lazyvim package installer config
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,7 +16,7 @@ require("vim-options")
 require("lazy").setup("plugins")
 
 -- Load all plugin configurations from 'plugins' directory
-local plugin_configs = vim.fn.stdpath('config') .. '/plugins/*.lua'
+local plugin_configs = vim.fn.stdpath("config") .. "/plugins/*.lua"
 for _, file in ipairs(vim.fn.glob(plugin_configs, true, true)) do
-  dofile(file)
+	dofile(file)
 end
