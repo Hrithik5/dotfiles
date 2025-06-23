72ef8d6dfe39244f0cffa4059b8d29bbea0ea623
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2025-06-23 20:22:34 +0530

    Modifies none-ls config for es_lint warning

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,26 +6,41 @@ return {
 	},
 	config = function()
 		local none_ls = require("null-ls")
+
+		-- Utility function to check if a binary exists
+		local function has_bin(bin)
+			return vim.fn.executable(bin) == 1
+		end
+
+		local sources = {
+			-- Always included formatters
+			none_ls.builtins.formatting.stylua,
+			none_ls.builtins.formatting.black,
+			none_ls.builtins.formatting.isort,
+			none_ls.builtins.formatting.yamlfix,
+			none_ls.builtins.formatting.goimports_reviser,
+			none_ls.builtins.formatting.golines,
+			none_ls.builtins.formatting.gofumpt,
+
+			-- Completion
+			none_ls.builtins.completion.spell,
+		}
+
+		-- Conditionally load eslint_d if available
+		if has_bin("eslint_d") then
+			table.insert(sources, none_ls.builtins.formatting.eslint_d)
+			table.insert(sources, none_ls.builtins.diagnostics.eslint_d)
+		end
+
+		-- Conditionally load prettier if available
+		if has_bin("prettier") then
+			table.insert(sources, none_ls.builtins.formatting.prettier)
+		end
+
 		none_ls.setup({
-			sources = {
-				-- Formatting
-				none_ls.builtins.formatting.eslint_d,
-				none_ls.builtins.formatting.stylua,
-				none_ls.builtins.formatting.black,
-				none_ls.builtins.formatting.isort,
-				none_ls.builtins.formatting.yamlfix,
-				none_ls.builtins.formatting.prettier,
-				none_ls.builtins.formatting.goimports_reviser,
-				none_ls.builtins.formatting.golines,
-				none_ls.builtins.formatting.gofumpt,
-
-				-- Diagnostics
-				none_ls.builtins.diagnostics.eslint_d,
-
-				-- Completion
-				none_ls.builtins.completion.spell,
-			},
+			sources = sources,
 		})
+
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format Code" })
 	end,
 }
