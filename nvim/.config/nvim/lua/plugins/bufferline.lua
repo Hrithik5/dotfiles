72a03c518614640384f583108576501995fbba4d
Hrithik Chauhan <hrithikchauhan01@gmail.com>
2025-06-28 00:13:02 +0530

    Changes config for bufferline plugin

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,56 +1,77 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    vim.opt.termguicolors = true
-    local bufferline = require("bufferline")
-    bufferline.setup({
-      options = {
-        mode = "buffer",
-        style_preset = bufferline.style_preset.default,
-        indicator = {
-          icon = "▎", -- this should be omitted if indicator style is not 'icon'
-          style = "icon",
-        },
-        buffer_close_icon = "󰅖",
-        modified_icon = "●",
-        close_icon = "",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
-        color_icons = true, -- whether or not to add the filetype icon highlights
-        get_element_icon = function(element)
-          -- element consists of {filetype: string, path: string, extension: string, directory: string}
-          -- This can be used to change how bufferline fetches the icon
-          -- for an element e.g. a buffer or a tab.
-          -- e.g.
-          local icon, hl =
-              require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
-          return icon, hl
-        end,
-        show_buffer_icons = true, -- disable filetype icons for buffers
-        show_buffer_close_icons = true,
-        show_close_icon = true,
-        show_tab_indicators = true,
-        separator_style = "thick",
-        enforce_regular_tabs = false,
-        always_show_bufferline = true,
-        hover = {
-          enabled = true,
-          delay = 200,
-          reveal = { "close" },
-        },
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "File Explorer",
-            text_align = "left",
-            separator = true,
-          },
-        },
-      },
-    })
-  end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		vim.opt.termguicolors = true
+		local bufferline = require("bufferline")
+
+		bufferline.setup({
+			options = {
+				mode = "buffers",
+				style_preset = bufferline.style_preset.default,
+				indicator = {
+					icon = "▎",
+					style = "icon",
+				},
+				buffer_close_icon = "󰅖",
+				modified_icon = "●",
+				close_icon = "",
+				left_trunc_marker = "",
+				right_trunc_marker = "",
+				color_icons = true,
+				get_element_icon = function(element)
+					local icon, hl =
+						require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
+					return icon, hl
+				end,
+				show_buffer_icons = true,
+				show_buffer_close_icons = true,
+				show_close_icon = true,
+				show_tab_indicators = true,
+				separator_style = "thick",
+				enforce_regular_tabs = false,
+				always_show_bufferline = true,
+				hover = {
+					enabled = true,
+					delay = 200,
+					reveal = { "close" },
+				},
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "left",
+						separator = true,
+					},
+				},
+			},
+		})
+
+		-- Setup Telescope for Fuzzy Buffer Switching
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<esc>"] = actions.close,
+						["<c-d>"] = actions.delete_buffer, -- Optional: Delete buffer directly from picker
+					},
+					n = {
+						["<c-d>"] = actions.delete_buffer,
+					},
+				},
+			},
+		})
+
+		-- Keymap for Fuzzy Buffer Switching
+		vim.keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy Find Buffers" })
+	end,
 }
 
 -- :h bufferline-configuration [For configuration of the plugin]
